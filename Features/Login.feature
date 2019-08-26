2b9b2046bcc02f1092249a7f864afc54ec57116d
Rathish Sethumadhavan <rathishs691@gmail.com>
2019-08-26 12:32:11 +0530

    upload files

---
+++ Features/Login.feature
@@ -0,0 +1,5 @@
+Feature: Test facebook smoke
+Scenario: Test login
+Given Open chrome and Launch Application 
+When I enter valid credentials 
+And I click on login button
---
+++ src/test/java/jenkinsWithMaven/VerifyTitle.java
@@ -0,0 +1,18 @@
+package jenkinsWithMaven;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class VerifyTitle {
+
+	@Test
+	public void verifyvalidlogin()
+	{
+		System.setProperty("webdriver.chrome.driver", "F:\\Softwares\\Selenium\\Selenium Drivers\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.facebook.com");
+		System.out.println("Title is:"+driver.getTitle());
+		driver.close();
+	}
+}
